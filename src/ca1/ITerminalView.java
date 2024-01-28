a0177fc3b5980ef7148607ad118910f76502e1ac
KristianColville1 <kristiancolville1@gmail.com>
2024-01-28 13:28:06 +0000

    Created interface ITerminalView

---
+++ src/ca1/ITerminalView.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca1;
+
+/**
+ *
+ * @author kristian colville
+ * 
+ * This is an interface for menu handling in the terminal application.
+ * This interface defines the functionalities that any terminal view
+ * should implement.
+ */
+public interface ITerminalView {
+    /**
+     * Displays the menu options to the user.
+     */
+    void displayWelcomeOptions();
+}
