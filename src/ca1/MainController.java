99250ca0eeee43f045916465a733afb4e5e8d36a
KristianColville1 <kristiancolville1@gmail.com>
2024-01-28 12:28:22 +0000

    Added while loop and highest try catch block for application

--- src/ca1/MainController.java
+++ src/ca1/MainController.java
@@ -22,9 +22,27 @@ public class MainController {
         this.appRunning = true;
     }
     /**
-     * Responsible for running the application processes for the user
+     * Responsible for running the application processes for the user.
      */
     public void startApp(){
-        
+        // While loop that continues as long as appRunning is true
+        while(appRunning){
+            /**
+             * This is the highest try catch block in the application.
+             * Its designed to catch and handle any uncaught exceptions from 
+             * lower in the application.
+             * 
+             * This ensures the application can handle errors gracefully.
+             * 
+             * Helps to provide feedback for better exception handling lower
+             * down the application.
+             * 
+             */
+            try {
+                // Application Logic
+            } catch(Exception e){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
     }
 }
