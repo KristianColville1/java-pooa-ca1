224c6b4a6c08199a5cce14e9ceb7d3c031948635
KristianColville1 <kristiancolville1@gmail.com>
2024-01-28 14:19:26 +0000

    Set up Runnable for DataController to stop app

--- src/ca1/DataController.java
+++ src/ca1/DataController.java
@@ -12,16 +12,21 @@ package ca1;
  */
 public class DataController implements IDataController {
 
-    private TerminalView menu;
+    private TerminalView menu; // TerminalMenu instance.
+    private Runnable exitApp; // Runnable to stop app.
 
     /**
      * Constructor for DataController.
      * 
      * @param menu is the TerminalView instance to the used for user interaction.
      * This dependency is injected to separate concerns.
+     * @param exitApp is a Runnable that serves as an event listener to signal
+     * that the application should be exited.
+     * DataController posses the ability to terminate the application.
      */
-    public DataController(TerminalView menu) {
+    public DataController(TerminalView menu, Runnable exitApp) {
         this.menu = menu;
+        this.exitApp = exitApp;
     }
     
     @Override
