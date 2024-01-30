920176f3c84df6ad1c57d5179b283399fb052ba9
KristianColville1 <kristiancolville1@gmail.com>
2024-01-30 21:13:58 +0000

    Added comment for Member class inheritance of Person

--- src/ca1/Member.java
+++ src/ca1/Member.java
@@ -7,7 +7,14 @@ package ca1;
 /**
  *
  * @author kristian
+ *
+ * Abstract class that extends Person to include membership-specific
+ * information.
+ *
+ * This class demonstrates the use of inheritance to create a more specific
+ * abstraction from the Person class. Adds new properties and behaviours that
+ * are common among members.
  */
 public abstract class Member extends Person {
-    
+
 }
