2835f3a54eacbd952c1939403fcc7fbc410ea48c
KristianColville1 <kristiancolville1@gmail.com>
2024-01-30 19:36:45 +0000

    Created enum to represent student workload table

---
+++ src/ca1/StudentWorkload.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca1;
+
+/**
+ *
+ * @author kristian
+ * 
+ * StudentWorkload enum used for storing the constant data about the number
+ * of student classes and the corresponding workload.
+ */
+public enum StudentWorkload {
+    VERY_LIGHT(1, "Very Light"),
+    LIGHT(2, "Light"),
+    PART_TIME(3, "Part Time"),
+    FULL_TIME(6, "Full TIME");
+
+}
