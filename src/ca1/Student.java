a48b2629ebd036d41112aa96e204288a7e0df847
KristianColville1 <kristiancolville1@gmail.com>
2024-01-30 21:23:35 +0000

    Created final class Student that inherits Person and Member

---
+++ src/ca1/Student.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca1;
+
+/**
+ * The Student class is a concrete class that extends from the abstract Member
+ * class, representing a student with specific characteristics and behaviors.
+ *
+ * This class is final. As a final class in the inheritance chain, Student
+ * inherits properties from Person and Member and it is here that abstract
+ * methods are given implementation.
+ *
+ * @author kristian
+ */
+public final class Student extends Member {
+
+}
