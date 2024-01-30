70cff2e41e406190b501138898c8d8ff442648c6
KristianColville1 <kristiancolville1@gmail.com>
2024-01-30 19:40:56 +0000

    Create constructor and variables for the StudentWorkload enum

--- src/ca1/StudentWorkload.java
+++ src/ca1/StudentWorkload.java
@@ -7,9 +7,9 @@ package ca1;
 /**
  *
  * @author kristian
- * 
- * StudentWorkload enum used for storing the constant data about the number
- * of student classes and the corresponding workload.
+ *
+ * StudentWorkload enum used for storing the constant data about the number of
+ * student classes and the corresponding workload.
  */
 public enum StudentWorkload {
     VERY_LIGHT(1, "Very Light"),
@@ -17,4 +17,18 @@ public enum StudentWorkload {
     PART_TIME(3, "Part Time"),
     FULL_TIME(6, "Full TIME");
 
+    private final int classes;
+    private final String description;
+
+    /**
+     * Constructor StudentWorkload
+     *
+     * @param classes represents the number of classes associated with a
+     * workload.
+     * @param description provides information about the workload.
+     */
+    StudentWorkload(int classes, String description) {
+        this.classes = classes;
+        this.description = description;
+    }
 }
