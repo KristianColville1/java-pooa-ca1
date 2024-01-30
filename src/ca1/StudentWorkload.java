304439db36ea448d43ad24fbccb8831af20fb1d7
KristianColville1 <kristiancolville1@gmail.com>
2024-01-30 20:06:43 +0000

    Created method to determine the student workload constant created

--- src/ca1/StudentWorkload.java
+++ src/ca1/StudentWorkload.java
@@ -21,7 +21,7 @@ public enum StudentWorkload {
     private final String description;
 
     /**
-     * Constructor StudentWorkload
+     * Constructor StudentWorkload sets up constants.
      *
      * @param classes represents the number of classes associated with a
      * workload.
@@ -34,10 +34,39 @@ public enum StudentWorkload {
 
     /**
      * Gets the description for a number of classes.
+     *
      * @return the associated description for the number of classes for a
      * particular constant.
      */
     public String getDescription() {
         return description;
+    }
+
+    /**
+     * Determines and sets the workload based on the number of classes.
+     *
+     * @param numOfClasses the number of classes a student is taking.
+     * @return associated description
+     * @throws IllegalArgumentException if numOfClasses is less than 1 or higher
+     * than 8.
+     */
+    public StudentWorkload setWorkload(int numOfClasses) {
+        if (numOfClasses < 1 || numOfClasses > 8) {
+            throw new IllegalArgumentException(
+                    "Number of classes must be greater"
+                    + " than 0 and less than 8 inclusively.");
+        }
+        switch (numOfClasses) {
+            case 1:
+                return VERY_LIGHT; // covers 1 class
+            case 2:
+                return LIGHT; // covers 2 classes
+            case 3:
+            case 4:
+            case 5:
+                return PART_TIME; // covers 3, 4 & 5 classes
+            default:
+                return FULL_TIME; // covers 6 classes or higher
+        }
     }
 }
