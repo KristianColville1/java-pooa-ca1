df59e8d0d63cd8179d465b2bb33fcb930de3bfe4
KristianColville1 <kristiancolville1@gmail.com>
2024-02-03 15:46:27 +0000

    Created array lists for storing valid and invalid students in StudentRepository

--- src/ca1/StudentRepository.java
+++ src/ca1/StudentRepository.java
@@ -4,10 +4,31 @@
  */
 package ca1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * StudentRepository class holds two data structures called array lists.
+ *
+ * This class focuses on data retrieval and storage operations relating to
+ * student objects.
+ *
+ * These array lists separate the valid and invalid student objects that are
+ * created.
  *
  * @author kristian
  */
 public class StudentRepository {
-    
+
+    private List<Student> validStudents;
+    private List<Student> invalidStudents;
+
+    /**
+     * Constructs a new StudentData instance. Initializes empty lists for both
+     * valid and invalid students.
+     */
+    public StudentRepository(){
+        this.validStudents = new ArrayList<>();
+        this.invalidStudents = new ArrayList<>();
+    }
 }
