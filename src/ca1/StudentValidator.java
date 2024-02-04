d89fa82dba726bc322ad49df2ca0bdd953e4290f
KristianColville1 <kristiancolville1@gmail.com>
2024-02-04 15:17:01 +0000

    Refactor validation logic to the StudentValidator class

--- src/ca1/StudentValidator.java
+++ src/ca1/StudentValidator.java
@@ -4,10 +4,63 @@
  */
 package ca1;
 
+import java.util.List;
+
 /**
  *
  * @author kristian
  */
 public class StudentValidator {
-    
+
+    private final StudentRepository studentRepository;
+
+    public StudentValidator(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    /**
+     * Performs validation checks on student data and sets the valid status of
+     * these objects.
+     *
+     * @param data the list of student objects.
+     */
+    public void performValidationChecksOnStudentData(List<Student> data) {
+        // validate
+        for (Student student : data) {
+            /**
+             * if the students last name is blank if the first name contains
+             * only letters if the last name contains letters/numbers
+             */
+            if ("".equals(student.getLastName())
+                    || checkForLettersOnly(student.getFirstName())
+                    || checkForLettersAndNumbers(student.getLastName())) {
+                student.setValidStatus(false);
+                student.isInvalidBecause(
+                        "Student Name",
+                        "first and/or last name is invalid");
+            }
+
+            // validates the student ID
+            if (!validateTheStudentID(student)) {
+                student.setValidStatus(false);
+                student.isInvalidBecause(
+                        "Student ID",
+                        String.format(
+                                "student ID is invalid: %s",
+                                student.getIdentityNumber()));
+            }
+        }
+    }
+
+    public Boolean checkForLettersOnly(String data) {
+        return false;
+    }
+
+    public Boolean checkForLettersAndNumbers(String data) {
+        return false;
+    }
+
+    public Boolean validateTheStudentID(Student student) {
+        return false;
+    }
 }
