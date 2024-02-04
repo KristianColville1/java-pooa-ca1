909e375fe1e1d9f3679f9ffc924b3560105df3cb
KristianColville1 <kristiancolville1@gmail.com>
2024-02-04 19:53:24 +0000

    Implement abstract methods from IUserInput in StudentFileWriter

--- src/ca1/StudentFileWriter.java
+++ src/ca1/StudentFileWriter.java
@@ -9,15 +9,28 @@ import java.io.Writer;
 
 /**
  * Final class StudentFileWriter extends built in BufferedWriter.
- * 
+ *
  * Handles manual writing to file for user.
- * 
+ *
  * @author kristian
  */
-public final class StudentFileWriter extends BufferedWriter{
-    
+public final class StudentFileWriter extends BufferedWriter
+        implements IUserInput {
+
     public StudentFileWriter(Writer writer) {
         super(writer);
     }
-    
+
+    @Override
+    public void queryUser() {
+    }
+
+    @Override
+    public void confirmUserSelection() {
+    }
+
+    @Override
+    public void writeUsersSelectionToFile() {
+    }
+
 }
